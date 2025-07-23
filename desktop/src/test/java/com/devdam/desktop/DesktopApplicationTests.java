46f12f1eba013a17950905088e7f09218d9f81e9
Damoiskii <moimyazz@gmail.com>
2025-07-23 15:33:37 -0500

    Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ desktop/src/test/java/com/devdam/desktop/DesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdam.desktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
